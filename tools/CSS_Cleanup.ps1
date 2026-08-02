f6b0a49a1abacf460fef1f11f2413fbf34536830
Okan Şengün <okan.sengun@gmail.com>
2026-08-02 17:56:31 +0300

    New Sensor Cards update

--- tools/CSS_Cleanup.ps1
+++
@@ -1,5 +0,0 @@
-# Find all classes in HTML
-Select-String -Path ..\web_assets\*.html -Pattern 'class="([^"]*)"' -AllMatches > styles_in_html.txt
-
-# Find all CSS definitions
-Select-String -Path ..\web_assets\*.css -Pattern '\.[a-zA-Z0-9_-]+' > css_styles.txt
